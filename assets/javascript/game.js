2b50c240dbd68c05528900b02330ad89b11955dc
Nicolas Cueva <nicolascueva@outlook.com>
2018-08-05 16:42:06 -0500

    Added some comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,29 +3,17 @@ var losses = 0;
 var guessesLeft = 9;
 var letterLog = [];
 
-
-// document.getElementById("guessesLeft");
-// guessesLeft.textContent;
-
-/*document.getElementById("guessesLeft");
-guessesLeft.textContent = guessesLeft;
-var guessesLeft = 9;*/
-
-/*var guessesLeft = document.getElementById("guessesLeft");
-guessesLeft.textContent = guessesLeft;*/
+// alphabet for computer generated random letter
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
-
-
+// begin function: everything triggers when user presses a button
 document.onkeyup = function (event) {
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(computerChoice)
-
+    //above generates random number
 
     var letterLog = document.getElementById("letterLog");
-    //var letter = event.key.toLowerCase();
     letterLog.textContent += event.key;
     // Above adds each letter typed to the guesses so far
 
@@ -33,49 +21,24 @@ document.onkeyup = function (event) {
     var userGuess = event.key;
 
 
-
+    //if else statements begin
+    // if they win then
     if (userGuess === computerChoice) {
         wins++;
         guessesLeft = 9;
         letterLog.textContent = "";
-        //letterLog = "";
+        // if they dont guess correct
     } else {
         guessesLeft--;
     }
-
+    // adds loss when they lose the game
     if (guessesLeft === 0) {
         losses++;
         guessesLeft = 9;
         letterLog.textContent = "";
-        /*function empty() {
-            letterLog.length = 0;
-        }
-        empty();*/
-
-        /*for (var i = letterLog.length; i > 0; i--) {
-            letterLog.pop();
-        }*/
     }
-
-
-
+    // adds/changes html to html document
     document.getElementById('wins').innerHTML = wins;
     document.getElementById('losses').innerHTML = losses;
     document.getElementById('guessesLeft').innerHTML = guessesLeft;
-
-
-
-
-    /*function updateGuesses() {
-        document.getElementById("guessesLeft").textContent = guessesLeft;
-    }
-    guessesLeft--;
-    updateGuesses();*/
-
-
-
-    /*guessesLeft--;
-    document.getElementById("guessesLeft");
-    guessesLeft.textContent = guessesLeft;*/
-
 };
